ca4aa3b31f0e84b0efcca17e07a56c620ea9277a
catherine91 <70351707+catherine91@users.noreply.github.com>
2020-11-23 22:29:10 -0700

    changes made by Catherine

---
+++ Functions.js
@@ -0,0 +1,15 @@
+function addTwoNumber(x, y) { 
+  return x + y;
+}
+
+function returnLargest(x, y, z) {
+  return Math.max(x, y, z);
+}
+
+function evenOrOdd(x) {
+  if (x % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
